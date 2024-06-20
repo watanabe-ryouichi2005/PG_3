7ab528726cdaed795351e025746e521f394f139b
ワタナベリョウイチ <146901519+watanabe-ryouichi2005@users.noreply.github.com>
2024-06-20 17:17:08 +0900

    ヒントでかすぎて感動

---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include<stdio.h>
+
+
+class Parent
+{
+public:
+	 virtual void Voice() {
+		printf("あにまる¥n");
+	};
+};
+
+class ChildA : public Parent
+{
+public:
+	void Voice() {
+		printf("子供Aっす\n");
+	};
+};
+
+class ChildB : public Parent
+{
+public:
+	void Voice() {
+		printf("子供Bっす\n");
+	};
+};
+
+int main(void) {
+
+	Parent* animal[2] = { new ChildA,new ChildB };
+
+	animal[0]->Voice();
+	animal[1]->Voice();
+
+	delete animal[0];
+	delete animal[1];
+
+	return 0;
+}
